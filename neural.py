4c861476cb72422ea184f2d63112664628e469fa
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2020-01-27 01:28:28 -0300

    The neural network was created.

---
+++ neural.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+training_inputs = np.array([[0,0,1],
+                            [1,1,1],
+                            [1,0,1],
+                            [0,1,1]])
+
+training_outputs = np.array([[0,1,1,0]]).T
+
+np.random.seed(1)
+
+synaptic_weights = 2 * np.random.random((3, 1)) - 1 
+
+print('Random starting sunaptin weights: ')
+print(synaptic_weights)
+
+for iteration in range(1):
+
+    input_layer = training_inputs
+
+    outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+
+print('Outputs after training: ')
+print(outputs)
