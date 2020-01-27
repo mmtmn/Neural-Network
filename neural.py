57a61df57fd7e791d921e6633f7ac00d9a5ff273
thiago <thiagomnobrega@gmail.com>
2020-01-27 01:48:17 -0300

    Update neural.py

--- neural.py
+++ neural.py
@@ -3,6 +3,9 @@ import numpy as np
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
 training_inputs = np.array([[0,0,1],
                             [1,1,1],
                             [1,0,1],
@@ -14,7 +17,7 @@ np.random.seed(1)
 
 synaptic_weights = 2 * np.random.random((3, 1)) - 1 
 
-print('Random starting sunaptin weights: ')
+print('Random starting synaptic weights: ')
 print(synaptic_weights)
 
 for iteration in range(1):
@@ -22,6 +25,15 @@ for iteration in range(1):
     input_layer = training_inputs
 
     outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+
+    error = training_outputs - outputs
+
+    adjustments = error * sigmoid_derivative(outputs)
+
+    synaptic_weights += np.dot(input_layer.T, adjustments)
+
+print('Synaptic weights after training')
+print(synaptic_weights)
 
 print('Outputs after training: ')
 print(outputs)
